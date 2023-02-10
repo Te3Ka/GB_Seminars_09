c15a82d31594106550ba981437fbc7c17bedba47
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-10 20:48:06 +0300

    Задание 1 и 2. Выполнено

---
+++ Exercise01/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+void PrintNumber(int N)
+{
+    if (N <= 0)
+    {
+        return;
+    }
+    else if (N == 1)
+    {
+        Console.Write($"{N}");
+    }
+    else
+    {
+        PrintNumber(N - 1);
+        Console.Write($", {N}");
+    }
+}
+
+int GetNumber()
+{
+    Console.Write($"Введите значение числа: ");
+    int.TryParse(Console.ReadLine(), out int result);
+    return result;
+}
+
+int num = GetNumber();
+PrintNumber(num);
---
+++ Exercise02/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8"
+*/
+
+void PrintNumber(int M, int N)
+{
+    if (M > N)
+    {
+        PrintNumber(M - 1, N);
+        Console.Write($", {M}");
+    }
+    else if (M < N)
+    {
+        PrintNumber(M, N - 1);
+        Console.Write($", {N}");
+    }
+    else if (M == N)
+    {
+        Console.Write($"{M}");
+    }
+}
+
+int GetNumber()
+{
+    Console.Write($"Введите значение числа: ");
+    int.TryParse(Console.ReadLine(), out int result);
+    return result;
+}
+
+int M = GetNumber();
+int N = GetNumber();
+PrintNumber(M, N);
