3f8359e45d79687830df1740f6a572933c5dae86
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-10 21:33:58 +0300

    Задание 3 и 4. Выполнено

---
+++ Exercise03/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+int SumDigits(int num)
+{
+    if (num < 10)
+    {
+        return num;
+    }
+    else
+    {
+        return num % 10 + SumDigits(num / 10);
+    }
+}
+
+int GetNumber()
+{
+    Console.Write($"Введите значение числа: ");
+    int.TryParse(Console.ReadLine(), out int result);
+    return result;
+}
+
+int number = Math.Abs(GetNumber());
+Console.WriteLine($"Сумма цифр числа {number} = {SumDigits(number)}");
---
+++ Exercise04/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа A и B,
+и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+*/
+
+double PowNumbers(double numA, double numB)
+{
+    if (numB == 1)
+    {
+        return numA;
+    }
+    else if (numB == 0)
+    {
+        return 1;
+    }
+    else if (numB < 0)
+    {
+        return 1 / PowNumbers(numA, Math.Abs(numB));
+    }
+    else
+    {
+        return numA * PowNumbers(numA, numB - 1);
+    }
+}
+
+double GetNumber()
+{
+    Console.Write($"Введите значение числа: ");
+    double.TryParse(Console.ReadLine(), out double result);
+    return result;
+}
+
+double numberA = GetNumber();
+double numberB = GetNumber();
+    Console.WriteLine($"Число {numberA} в степени {numberB} = {PowNumbers(numberA, numberB)}");
